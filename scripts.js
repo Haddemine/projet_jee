91840ec530ff50d34569f297aa975a7a059548f8
Haddemine <103125248+Haddemine@users.noreply.github.com>
2022-07-03 21:57:30 +0000

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function ValiderMail(formulaire) {
-     if(formulaire.mail.value===null){
-      alert("bonjour");
-     }
-     else if (formulaire.mail.value.indexOf("@gmail.com",0)<0) {alert("Adresse mail saisie invalide.")}
-     else {
-        alert("Bon");
-     }
-  }
